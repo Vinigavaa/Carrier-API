a6cf9fd767b5305d6ba278d59c3828309ea2c389
Zer0nights <137710927+Zer0nights@users.noreply.github.com>
2024-11-26 15:14:44 -0300

    First Commit

---
+++ Gereciamento/src/main/java/com/apiTransporte/Gereciamento/GereciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.apiTransporte.Gereciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GereciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GereciamentoApplication.class, args);
+	}
+
+}
---
+++ Gereciamento/src/test/java/com/apiTransporte/Gereciamento/GereciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiTransporte.Gereciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GereciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
