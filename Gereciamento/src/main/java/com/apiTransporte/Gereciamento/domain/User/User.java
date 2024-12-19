be163c92e14d7f8dcec6a50f8e7c5b4858183ac1
Vinigavaa <vinigava14@gmail.com>
2024-12-19 14:25:05 -0300

    feat: setting securityfilterchain config

--- Gereciamento/src/main/java/com/apiTransporte/Gereciamento/domain/User/User.java
+++ Gereciamento/src/main/java/com/apiTransporte/Gereciamento/domain/User/User.java
@@ -1,7 +1,6 @@
 package com.apiTransporte.Gereciamento.domain.User;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,6 +17,8 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String login;
--- Gereciamento/src/main/java/com/apiTransporte/Gereciamento/infra/security/SecurityConfigurations.java
+++ Gereciamento/src/main/java/com/apiTransporte/Gereciamento/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.apiTransporte.Gereciamento.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -9,12 +10,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+//configs gerais para a auth
 public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity // Use o objeto httpSecurity
                 .csrf(csrf -> csrf.disable()) // Configuração do CSRF
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sessão Stateless
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/vehicle").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build(); // Constrói o SecurityFilterChain
     }
 }
